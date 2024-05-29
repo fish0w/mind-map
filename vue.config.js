2d49675a04d0b066674bc5f95606ba56146f3573
Fish0w <62744442+fish0w@users.noreply.github.com>
2024-05-29 21:52:23 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
+const fs = require('fs');
+
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/md_edit/' // 替换为你的子路径
+    : '/',
   devServer: {
+    https: {
+      key: fs.readFileSync('/etc/letsencrypt/live/gptall.online/privkey.pem'), // 替换为你的私钥文件路径
+      cert: fs.readFileSync('/etc/letsencrypt/live/gptall.online/fullchain.pem'), // 替换为你的证书文件路径
+    },
     host: '0.0.0.0',
-    port: 8080
-  }
-}
+    port: 8080,
+  },
+};
