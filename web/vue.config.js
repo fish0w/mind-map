cdb2bc231fc10e52d85da7f439b23fe507da5a3d
Fish0w <62744442+fish0w@users.noreply.github.com>
2024-05-29 22:35:46 +0800

    Update vue.config.js

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,27 +1,16 @@
 const path = require('path')
-const fs = require('fs') // 引入 fs 模块
 const isDev = process.env.NODE_ENV === 'development'
 const isLibrary = process.env.NODE_ENV === 'library'
 
 const WebpackDynamicPublicPathPlugin = require('webpack-dynamic-public-path')
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/md_edit/' // 替换为你的子路径
-    : '/',
+  publicPath: isDev ? '' : './dist',
   outputDir: '../dist',
   lintOnSave: false,
   productionSourceMap: false,
   filenameHashing: false,
   transpileDependencies: ['yjs', 'lib0'],
-  devServer: {
-    https: {
-      key: fs.readFileSync('/etc/letsencrypt/live/gptall.online/privkey.pem'), // 替换为你的私钥文件路径
-      cert: fs.readFileSync('/etc/letsencrypt/live/gptall.online/fullchain.pem'), // 替换为你的证书文件路径
-    },
-    host: '0.0.0.0',
-    port: 8080,
-  },
   chainWebpack: config => {
     // 移除 preload 插件
     config.plugins.delete('preload')
