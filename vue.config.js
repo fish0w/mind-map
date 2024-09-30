2519dcda6922e7ec74bbab2708016985729a4188
Fish0w <62744442+fish0w@users.noreply.github.com>
2024-09-30 13:32:38 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/md_edit/' // 替换为你的子路径
-    : '/',
+  publicPath: './',
   devServer: {
     headers: {
     'Access-Control-Allow-Origin': '*',
