5bb87dbac1ce4cbdc885c72590fcb081a5436f05
Fish0w <62744442+fish0w@users.noreply.github.com>
2024-09-30 13:24:01 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,18 +5,10 @@ module.exports = {
     ? '/md_edit/' // 替换为你的子路径
     : '/',
   devServer: {
-    headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
-  allowedHosts: 'all',
-  client: {
-    webSocketURL: 'wss://shuitunai.cn/sockjs-node',  // 设置正确的 WebSocket 地址，使用 wss 协议
-  },
     https: {
       key: fs.readFileSync('/etc/letsencrypt/live/shuitunai.cn/privkey.pem'), // 替换为你的私钥文件路径
       cert: fs.readFileSync('/etc/letsencrypt/live/shuitunai.cn/fullchain.pem'), // 替换为你的证书文件路径
     },
-
     host: '0.0.0.0',
     port: 8080,
   },
